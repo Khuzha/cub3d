8834c57d5014cfca7e4eb4ac39b23e34475eb783
khuzha <mr.khuzha@gmail.com>
2021-02-14 04:30:31 +0300

    remove extra loop

--- srcs/map_validator.c
+++ srcs/map_validator.c
@@ -6,14 +6,8 @@ char	get_line_type(t_map *map, char **arr)
 {
 	size_t i = 0;
 
-	while (arr[i])
-	{
-		if (arr[i][0] == 'R' && !arr[i][1] && ft_isnumber(arr[i + 1]) && ft_isnumber(arr[i + 2]))
-			fill_resolution(map, ft_atoi(arr[i + 1]), ft_atoi(arr[i + 2]));
-		i++;
-	}
-
-
+	if (arr[i][0] == 'R' && !arr[i][1] && ft_isnumber(arr[i + 1]) && ft_isnumber(arr[i + 2]) && !arr[i + 3])
+		fill_resolution(map, ft_atoi(arr[i + 1]), ft_atoi(arr[i + 2]));
 
 	return (0);
 }
@@ -23,12 +17,7 @@ int		validate_map(t_map *map)
 	while (map->list->next)
 	{
 		get_line_type(map, ft_split(map->list->content, ' '));
-		printf("\n");
-		// char **arr = ft_split(map_list->content, ' ');
-
-		// printf("type = %c, str = |%s|\n", get_line_type(map_list->content), map_list->content);
 		map->list = map->list->next;
 	}
-	printf("res_x = %d, res_y = %d", map->res_x, map->res_y);
 	return (1);
 }
