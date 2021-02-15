85d4f218924aeb68acf0c52535da79688fba0973
khuzha <mr.khuzha@gmail.com>
2021-02-15 15:33:57 +0300

    remove printfs

--- main.c
+++ main.c
@@ -12,13 +12,10 @@ int	main(int argc, char **argv)
 		return (-1);
 	init_str_and_map(&str, map);
 	fd = open(argv[1], O_RDONLY);
-	printf("bef gnl\n");
 	while(get_next_line(fd, &str) > 0)
 	{
-		printf("1\n");
 		ft_lstadd_back(&map->list, ft_lstnew(str));
 	}
 	ft_lstadd_back(&map->list, ft_lstnew(str));
-	printf("bef validate_map\n");
 	validate_map(map);
 }
