1484736d70efd2601baf6aa83ccf6e1428d3d4e9
khuzha <mr.khuzha@gmail.com>
2021-02-28 21:45:08 +0300

    tmp

--- srcs/map/validation.c
+++ srcs/map/validation.c
@@ -29,10 +29,19 @@ void	find_player(char **arr, size_t *pos_x, size_t *pos_y)
 	printf("found %d players\n", (int)counter);
 }
 
-// void	flood_fill(char **arr, size_t x, size_t y)
-// {
-
-// }
+void	flood_fill(char **arr, size_t x, size_t y)
+{
+	if (x >= 0 && y >= 0 && (arr[y][x] == '0' || ft_strchr("WENS", arr[y][x])))
+	{
+		flood_fill(arr, x + 1, y + 1);
+		flood_fill(arr, x + 1, y);
+		flood_fill(arr, x + 1, y - 1);
+		flood_fill(arr, x, y - 1);
+		flood_fill(arr, x - 1, y - 1);
+		flood_fill(arr, x - 1, y);
+		flood_fill(arr, x - 1, y + 1);
+	}
+}
 
 void	validate_map(char **arr, t_map *data)
 {
@@ -40,5 +49,6 @@ void	validate_map(char **arr, t_map *data)
 	size_t	y;
 
 	find_player(arr, &x, &y);
+	flood_fill(arr, x, y);
 	data++;
 }
