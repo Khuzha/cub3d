67585cfe1f600a04c8a35ebeeb9bbbc8c7548c8d
khuzha <mr.khuzha@gmail.com>
2021-02-28 21:30:11 +0300

    add player position finding

--- srcs/map/validation.c
+++ srcs/map/validation.c
@@ -1,27 +1,44 @@
 #include "../../cub.h"
 
-void	check_players_count(char **arr)
+void	find_player(char **arr, size_t *pos_x, size_t *pos_y)
 {
+	size_t	x;
+	size_t	y;
 	char	counter;
 
+	x = 0;
+	y = 0;
 	counter = 0;
-	while (*arr)
+	while (arr[y])
 	{
-		while (**arr)
+		while (arr[y][x])
 		{
-			if (ft_strchr("WENS", **arr))
+			if (ft_strchr("WENS", arr[y][x]))
+			{
+				*pos_x = x;
+				*pos_y = y;
 				counter++;
+			}
 			if (counter > 1)
 				error("Wrong players count");
-			(*arr)++;
+			x++;
 		}
-		arr++;
+		x = 0;
+		y++;
 	}
 	printf("found %d players\n", (int)counter);
 }
 
+// void	flood_fill(char **arr, size_t x, size_t y)
+// {
+
+// }
+
 void	validate_map(char **arr, t_map *data)
 {
-	check_players_count(arr);
+	size_t	x;
+	size_t	y;
+
+	find_player(arr, &x, &y);
 	data++;
 }
