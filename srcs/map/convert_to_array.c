6fdf5d20135ca10922704ac47ec3410e2091bec8
khuzha <mr.khuzha@gmail.com>
2021-02-28 15:38:31 +0300

    tmp

--- srcs/map/convert_to_array.c
+++ srcs/map/convert_to_array.c
@@ -27,14 +27,44 @@ size_t	get_height(t_list *list)
 	return (counter);
 }
 
+void	init_lines(char **arr, size_t width)
+{
+	size_t	i;
+
+	while (*arr)
+	{
+		if (!(*arr = malloc(sizeof(char) * width)))
+			error("Malloc error");
+		i = 0;
+		while (i < width)
+		{
+			*arr[i] = 0;
+			i++;
+		}
+		arr++;
+	}
+}
+
+void	fill_lines(char **arr, t_list *list)
+{
+	while (*arr)
+	{
+		printf()
+	}
+}
+
 void	convert_to_array(t_list *list, t_map *data)
 {
 	size_t	width;
 	size_t	height;
-	printf("convert_to_array, str = %s\n", list->content);
-	
+	char	**arr;
+
 	height = get_height(list);
 	width = get_width(list, height);
+	if (!(arr = malloc(sizeof(char *) * height)))
+		error("Malloc error");
+	init_lines(arr, width);
+	fill_lines(arr, list);
 	printf("height = %lu, width = %lu\n", height, width);
 	data++;
 	width = 0;
