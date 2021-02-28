0ad230f188f7c915adfb7255f77dae46d376b7cf
khuzha <mr.khuzha@gmail.com>
2021-02-28 21:04:19 +0300

    add players count validation

--- srcs/map/validation.c
+++ srcs/map/validation.c
@@ -1,16 +1,27 @@
 #include "../../cub.h"
 
-size_t	count_players(char **arr)
+void	check_players_count(char **arr)
 {
-	size_t	counter;
+	char	counter;
 
 	counter = 0;
-	// while (*arr)
+	while (*arr)
+	{
+		while (**arr)
+		{
+			if (ft_strchr("WENS", **arr))
+				counter++;
+			if (counter > 1)
+				error("Wrong players count");
+			(*arr)++;
+		}
+		arr++;
+	}
+	printf("found %d players\n", (int)counter);
 }
 
 void	validate_map(char **arr, t_map *data)
 {
-	if (count_players(arr) != 1)
-		error("Wrong players number");
+	check_players_count(arr);
 	data++;
 }
