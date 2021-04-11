4ea14c36812b81d983c1521c620cb1e3ca5b2911
khuzha <mr.khuzha@gmail.com>
2021-04-11 21:43:28 +0300

    add quicksort

--- srcs/utils/quicksort.c
+++ srcs/utils/quicksort.c
@@ -6,8 +6,9 @@ void	quicksort(t_sprite *arr, int first, int last)
 	int 	right;
 	double 	middle;
 	double	tmp;
-
-	// printf("1: dist = %f, ind = %d\n", arr[(left + left) / 2].dist, (left + left) / 2);
+	left = first;
+	right = last;
+	// printf("1: dist = %f, ind = %d\n", arr[(left + right) / 2].dist, (left + right) / 2);
 	// printf("2: dist = %f, ind = %d\n", arr[(first + last) / 2].dist, (first + last) / 2);
 	// printf("szt: dist = %f, ind = %lu\n", arr[(size_t)(((size_t)first + (size_t)last) / 2)].dist, (size_t)((first + last) / 2));
 	// size_t i = first;
@@ -17,19 +18,20 @@ void	quicksort(t_sprite *arr, int first, int last)
 	// 	i++;
 	// }
 
-	if (first > last)
+	// printf("left = %d, right = %d\n", left, right);
+	if (first >= last)
 		return ;
 	left = first;
 	right = last;
-	middle = arr[(size_t)((left + right) / 2)].dist;
-	printf("left = %d, right = %d, middle (pos %d) = %f\n", left, right, (left + right) / 2, middle);
+	middle = arr[(left + right) / 2].dist;
+	// printf("left = %d, right = %d, middle (pos %d) = %f\n", left, right, (left + right) / 2, middle);
 	while (left <= right)
 	{
-		printf("left = %d, l value = %f, middle = %f\n", left, arr[left].dist, middle);
+		// printf("left = %d, l value = %f, middle = %f\n", left, arr[left].dist, middle);
 		while (arr[left].dist < middle)
 			left++;
 		while (arr[right].dist > middle)
-			right++;
+			right--;
 		if (left <= right)
 		{
 			tmp = arr[left].dist;
