decd080b15a90a126295bd95f3b65aae86ab8697
khuzha <mr.khuzha@gmail.com>
2021-04-24 20:08:44 +0300

    norm: start.c

--- srcs/gui/start.c
+++ srcs/gui/start.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   start.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zskeeter <zskeeter@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/24 20:08:31 by zskeeter          #+#    #+#             */
+/*   Updated: 2021/04/24 20:08:31 by zskeeter         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../cub.h"
 
 void	prepare_rc(t_rc *rc)
@@ -5,22 +17,26 @@ void	prepare_rc(t_rc *rc)
 	if (rc->ray_dir.x < 0)
 	{
 		rc->ray_step.x = -1;
-		rc->closest_line.x = (rc->player_pos.x - rc->ray_square.x) * rc->next_line.x;
+		rc->closest_line.x = (rc->player_pos.x - rc->ray_square.x)
+			* rc->next_line.x;
 	}
 	else
 	{
 		rc->ray_step.x = 1;
-		rc->closest_line.x = (rc->ray_square.x + 1 - rc->player_pos.x) * rc->next_line.x;
+		rc->closest_line.x = (rc->ray_square.x + 1 - rc->player_pos.x)
+			* rc->next_line.x;
 	}
 	if (rc->ray_dir.y < 0)
 	{
 		rc->ray_step.y = -1;
-		rc->closest_line.y = (rc->player_pos.y - rc->ray_square.y) * rc->next_line.y;
+		rc->closest_line.y = (rc->player_pos.y - rc->ray_square.y)
+			* rc->next_line.y;
 	}
 	else
 	{
 		rc->ray_step.y = 1;
-		rc->closest_line.y = (rc->ray_square.y + 1 - rc->player_pos.y) * rc->next_line.y;
+		rc->closest_line.y = (rc->ray_square.y + 1 - rc->player_pos.y)
+			* rc->next_line.y;
 	}
 }
 
@@ -43,11 +59,12 @@ void	run_dda(t_rc *rc, char **arr)
 		if (arr[rc->ray_square.y][rc->ray_square.x] == '1')
 			rc->was_hit = 1;
 	}
-
 	if (rc->side)
-		rc->dist_to_wall = (rc->ray_square.y - rc->player_pos.y + (1 - rc->ray_step.y) / 2) / rc->ray_dir.y;
+		rc->dist_to_wall = (rc->ray_square.y - rc->player_pos.y +
+			(1 - rc->ray_step.y) / 2) / rc->ray_dir.y;
 	else
-		rc->dist_to_wall = (rc->ray_square.x - rc->player_pos.x + (1 - rc->ray_step.x) / 2) / rc->ray_dir.x;
+		rc->dist_to_wall = (rc->ray_square.x - rc->player_pos.x +
+			(1 - rc->ray_step.x) / 2) / rc->ray_dir.x;
 }
 
 void	define_side(t_rc *rc)
@@ -80,7 +97,6 @@ void	init_windows(char **arr, t_map *data)
 	rc->data = data;
 	init_keys(rc);
 	init_mlx(rc);
-
 	drawer(rc);
 	draw_sprites(rc);
 	if (data->is_scnsht)
