ec3acb1b59e9dc968b38905942550a902480d65d
khuzha <mr.khuzha@gmail.com>
2021-04-24 20:12:48 +0300

    norm: handlers.c

--- srcs/gui/handlers.c
+++ srcs/gui/handlers.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handlers.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zskeeter <zskeeter@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/24 20:12:31 by zskeeter          #+#    #+#             */
+/*   Updated: 2021/04/24 20:12:32 by zskeeter         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../cub.h"
 
 void	handle_player_dir(t_rc *rc, t_map *data)
@@ -26,21 +38,19 @@ void	handle_player_dir(t_rc *rc, t_map *data)
 void	handle_rc(t_rc *rc, t_map *data, int x)
 {
 	rc->cam = 2 * x / (double)data->res.x - 1;
-
 	rc->ray_dir.y = rc->wens.y + rc->plane.y * rc->cam;
 	rc->ray_dir.x = rc->wens.x + rc->plane.x * rc->cam;
-
 	rc->ray_square.x = (int)rc->player_pos.x;
 	rc->ray_square.y = (int)rc->player_pos.y;
-
 	if (rc->ray_dir.y == 0)
 		rc->next_line.x = 0;
 	else
-		rc->next_line.x = rc->ray_dir.x != 0 ? ft_abs_dbl(1 / rc->ray_dir.x) : 1;
-
+		rc->next_line.x = rc->ray_dir.x != 0 ?
+			ft_abs_dbl(1 / rc->ray_dir.x) : 1;
 	if (rc->ray_dir.x == 0)
 		rc->next_line.y = 0;
 	else
-		rc->next_line.y = rc->ray_dir.y != 0 ? ft_abs_dbl(1 / rc->ray_dir.y) : 1;
+		rc->next_line.y = rc->ray_dir.y != 0 ?
+			ft_abs_dbl(1 / rc->ray_dir.y) : 1;
 	prepare_rc(rc);
 }
