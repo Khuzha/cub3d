14d2b651a4921f94004dbd4f23a9fc03af918ef0
khuzha <mr.khuzha@gmail.com>
2021-04-24 20:18:34 +0300

    norm: rotations.c

--- srcs/gui/rotations.c
+++ srcs/gui/rotations.c
@@ -1,28 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotations.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zskeeter <zskeeter@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/24 20:18:24 by zskeeter          #+#    #+#             */
+/*   Updated: 2021/04/24 20:18:25 by zskeeter         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../cub.h"
 
 void	rot_right(t_rc *rc)
 {
 	double	wens_y;
 	double	plane_y;
-	
-	wens_y = rc->wens.y;
-	rc->wens.y = rc->wens.y * cos(-rc->speed.rot) - rc->wens.x * sin(-rc->speed.rot);
-	rc->wens.x = wens_y * sin(-rc->speed.rot) + rc->wens.x * cos(-rc->speed.rot);
-	plane_y = rc->plane.y;
-	rc->plane.y = rc->plane.y * cos(-rc->speed.rot) - rc->plane.x * sin(-rc->speed.rot);
-	rc->plane.x = plane_y * sin(-rc->speed.rot) + rc->plane.x * cos(-rc->speed.rot);
 
+	wens_y = rc->wens.y;
+	rc->wens.y = rc->wens.y * cos(-rc->speed.rot) -
+		rc->wens.x * sin(-rc->speed.rot);
+	rc->wens.x = wens_y * sin(-rc->speed.rot) +
+		rc->wens.x * cos(-rc->speed.rot);
+	plane_y = rc->plane.y;
+	rc->plane.y = rc->plane.y * cos(-rc->speed.rot) -
+		rc->plane.x * sin(-rc->speed.rot);
+	rc->plane.x = plane_y * sin(-rc->speed.rot) +
+		rc->plane.x * cos(-rc->speed.rot);
 }
 
 void	rot_left(t_rc *rc)
 {
 	double	wens_y;
 	double	plane_y;
-	
+
 	wens_y = rc->wens.y;
-	rc->wens.y = rc->wens.y * cos(rc->speed.rot) - rc->wens.x * sin(rc->speed.rot);
-	rc->wens.x = wens_y * sin(rc->speed.rot) + rc->wens.x * cos(rc->speed.rot);
+	rc->wens.y = rc->wens.y * cos(rc->speed.rot) -
+		rc->wens.x * sin(rc->speed.rot);
+	rc->wens.x = wens_y * sin(rc->speed.rot) +
+		rc->wens.x * cos(rc->speed.rot);
 	plane_y = rc->plane.y;
-	rc->plane.y = rc->plane.y * cos(rc->speed.rot) - rc->plane.x * sin(rc->speed.rot);
-	rc->plane.x = plane_y * sin(rc->speed.rot) + rc->plane.x * cos(rc->speed.rot);
+	rc->plane.y = rc->plane.y * cos(rc->speed.rot) -
+		rc->plane.x * sin(rc->speed.rot);
+	rc->plane.x = plane_y * sin(rc->speed.rot) +
+		rc->plane.x * cos(rc->speed.rot);
 }
