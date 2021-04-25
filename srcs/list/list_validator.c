dd2621d2cb0fd4b2f836d6233eb4d79e8c127d03
khuzha <mr.khuzha@gmail.com>
2021-04-25 09:30:43 +0300

    norm: list_validator.c

--- srcs/list/list_validator.c
+++ srcs/list/list_validator.c
@@ -1,14 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list_validator.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zskeeter <zskeeter@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/25 09:29:31 by zskeeter          #+#    #+#             */
+/*   Updated: 2021/04/25 09:30:23 by zskeeter         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../cub.h"
 
 void	fill_struct(t_map *map, char **arr, char *str)
 {
 	if (!arr[0] && *str == ' ')
 		error("Invalid map (empty line contains spaces)");
-	if (arr[0] && arr[0][0] == 'R' && !arr[0][1] && ft_isnumber(arr[1]) && ft_isnumber(arr[2]) && !arr[3])
+	if (arr[0] && arr[0][0] == 'R' && !arr[0][1] &&
+		ft_isnumber(arr[1]) && ft_isnumber(arr[2]) && !arr[3])
 		fill_resolution(map, arr[1], arr[2]);
-	if ((is_wside(arr[0]) || (arr[0] && arr[0][0] == 'S')) && is_xpm_file(arr[1]) && !arr[2])
+	if ((is_wside(arr[0]) || (arr[0] && arr[0][0] == 'S'))
+		&& is_xpm_file(arr[1]) && !arr[2])
 		define_file(map, arr[0], arr[1]);
-	if (arr[0] && arr[0][0] && arr[1] && arr[0] && ft_strchr("CF", arr[0][0]) && !arr[0][1] && is_colors_set(str))
+	if (arr[0] && arr[0][0] && arr[1] && arr[0] &&
+		ft_strchr("CF", arr[0][0]) && !arr[0][1] && is_colors_set(str))
 		store_colors(map, arr[0][0], str);
 }
 
@@ -20,12 +35,13 @@ void	check_map(t_map *map)
 		!map->so || !ft_strlen(map->so) || !map->no || !ft_strlen(map->no) ||
 		!map->s || !ft_strlen(map->s) ||
 		!map->c_colors.defined || !map->f_colors.defined ||
-		map->c_colors.r > 255 || map->c_colors.g > 255 || map->c_colors.b > 255 ||
-		map->f_colors.r > 255 || map->f_colors.g > 255 || map->f_colors.b > 255)
+		map->c_colors.r > 255 || map->c_colors.g > 255 ||
+		map->c_colors.b > 255 || map->f_colors.r > 255 ||
+		map->f_colors.g > 255 || map->f_colors.b > 255)
 		error("Invalid map");
 }
 
-void		iterate_list(t_map *map)
+void	iterate_list(t_map *map)
 {
 	char	**arr;
 	char	was_map;
@@ -34,7 +50,8 @@ void		iterate_list(t_map *map)
 	while (map->list->next)
 	{
 		arr = ft_split(map->list->content, ' ');
-		if (arr[0] && !(is_param(arr[0]) && arr[1]) && !is_map_line(map->list->content))
+		if (arr[0] && !(is_param(arr[0]) && arr[1]) &&
+			!is_map_line(map->list->content))
 		{
 			was_map = 1;
 			check_map(map);
