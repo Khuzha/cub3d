e272d4d4a5e4996962e6a205f2540611ed3f145d
khuzha <mr.khuzha@gmail.com>
2021-04-25 13:39:15 +0300

    norm: init_sprites.c

--- srcs/map/init_sprites.c
+++ srcs/map/init_sprites.c
@@ -1,14 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_sprites.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zskeeter <zskeeter@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/25 13:37:42 by zskeeter          #+#    #+#             */
+/*   Updated: 2021/04/25 13:39:00 by zskeeter         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../cub.h"
 
-void	print_sprites(t_map *data, t_dxy player_pos)
+void		print_sprites(t_map *data, t_dxy player_pos)
 {
 	int i;
-	
+
 	i = 0;
-	printf("-----\nplayer position: y = %f, x = %f\n", player_pos.y, player_pos.x);
+	printf("-----\nplayer position: y = %f, x = %f\n",
+		player_pos.y, player_pos.x);
 	while (i < data->s_count)
 	{
-		printf("i = %d, sprite: y = %f, x = %f, dist = %f\n", i, data->sprites[i].y, data->sprites[i].x, data->sprites[i].dist);
+		printf("i = %d, sprite: y = %f, x = %f, dist = %f\n",
+			i, data->sprites[i].y, data->sprites[i].x,
+			data->sprites[i].dist);
 		i++;
 	}
 	printf("\n");
@@ -39,21 +54,26 @@ static void	calc_sprites(char **arr, t_sprite *sprites)
 	}
 }
 
-void	init_sprites(char **arr, t_map *data)
+void		init_sprites(char **arr, t_map *data)
 {
-	if (!(data->sprites = ft_calloc(sizeof(t_sprite) * data->s_count, sizeof(t_sprite) * data->s_count)))
+	if (!(data->sprites = ft_calloc(sizeof(t_sprite) *
+		data->s_count, sizeof(t_sprite) * data->s_count)))
 		error("Malloc error");
 	calc_sprites(arr, data->sprites);
 }
 
-void	recalc_sprites(t_rc *rc)
+void		recalc_sprites(t_rc *rc)
 {
 	int i;
-	
+
 	i = 0;
 	while (i < rc->data->s_count)
 	{
-		rc->data->sprites[i].dist = (rc->player_pos.x - rc->data->sprites[i].x) * (rc->player_pos.x - rc->data->sprites[i].x) + (rc->player_pos.y - rc->data->sprites[i].y) * (rc->player_pos.y - rc->data->sprites[i].y);
+		rc->data->sprites[i].dist =
+			(rc->player_pos.x - rc->data->sprites[i].x) *
+			(rc->player_pos.x - rc->data->sprites[i].x) +
+			(rc->player_pos.y - rc->data->sprites[i].y) *
+			(rc->player_pos.y - rc->data->sprites[i].y);
 		i++;
 	}
 }
