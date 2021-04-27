31b216754efe4fcf56e1c7dc14a161ff89505fd5
khuzha <mr.khuzha@gmail.com>
2021-04-27 20:07:41 +0300

    move xpm files checking to function

--- srcs/gui/initions.c
+++ srcs/gui/initions.c
@@ -6,7 +6,7 @@
 /*   By: zskeeter <zskeeter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/24 20:14:21 by zskeeter          #+#    #+#             */
-/*   Updated: 2021/04/26 21:59:03 by zskeeter         ###   ########.fr       */
+/*   Updated: 2021/04/27 20:07:15 by zskeeter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,18 +44,22 @@ void		free_file_paths(t_map *data)
 	free(data->s);
 }
 
+void		check_txtr(void	*ptr)
+{
+	if (!ptr)
+		error("Invalid file entity");
+}
+
 static void	init_txtr_imgs_2(t_rc *rc)
 {
 	rc->t.we.img.ptr = mlx_xpm_file_to_image(rc->mlx,
 		rc->data->we, &rc->t.we.h, &rc->t.we.w);
-	if (!rc->t.we.img.ptr)
-		error("Invalid file entity (we)");
+	check_txtr(rc->t.we.img.ptr);
 	rc->t.we.img.addr = mlx_get_data_addr(rc->t.we.img.ptr,
 		&rc->t.we.img.bpp, &rc->t.we.img.length, &rc->t.we.img.endian);
 	rc->t.s.img.ptr = mlx_xpm_file_to_image(rc->mlx,
 		rc->data->s, &rc->t.s.h, &rc->t.s.w);
-	if (!rc->t.s.img.ptr)
-		error("Invalid file entity (s)");
+	check_txtr(rc->t.s.img.ptr);
 	rc->t.s.img.addr = mlx_get_data_addr(rc->t.s.img.ptr,
 		&rc->t.s.img.bpp, &rc->t.s.img.length, &rc->t.s.img.endian);
 	free_file_paths(rc->data);
@@ -65,20 +69,17 @@ static void	init_txtr_imgs(t_rc *rc)
 {
 	rc->t.so.img.ptr = mlx_xpm_file_to_image(rc->mlx,
 		rc->data->so, &rc->t.so.h, &rc->t.so.w);
-	if (!rc->t.so.img.ptr)
-		error("Invalid file entity (so)");
+	check_txtr(rc->t.so.img.ptr);
 	rc->t.so.img.addr = mlx_get_data_addr(rc->t.so.img.ptr,
 		&rc->t.so.img.bpp, &rc->t.so.img.length, &rc->t.so.img.endian);
 	rc->t.no.img.ptr = mlx_xpm_file_to_image(rc->mlx,
 		rc->data->no, &rc->t.no.h, &rc->t.no.w);
-	if (!rc->t.no.img.ptr)
-		error("Invalid file entity (no)");
+	check_txtr(rc->t.no.img.ptr);
 	rc->t.no.img.addr = mlx_get_data_addr(rc->t.no.img.ptr,
 		&rc->t.no.img.bpp, &rc->t.no.img.length, &rc->t.no.img.endian);
 	rc->t.ea.img.ptr = mlx_xpm_file_to_image(rc->mlx,
 		rc->data->ea, &rc->t.ea.h, &rc->t.ea.w);
-	if (!rc->t.ea.img.ptr)
-		error("Invalid file entity (ea)");
+	check_txtr(rc->t.ea.img.ptr);
 	rc->t.ea.img.addr = mlx_get_data_addr(rc->t.ea.img.ptr,
 		&rc->t.ea.img.bpp, &rc->t.ea.img.length, &rc->t.ea.img.endian);
 	init_txtr_imgs_2(rc);
